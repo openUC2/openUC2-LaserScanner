8dc2758428e7b788a56f21e585b6c8b38bbd3565
beniroquai <bene.d@gmx.de>
2025-10-13 22:17:08 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -25,11 +25,11 @@ void app_main()
   esp_task_wdt_delete(xTaskGetIdleTaskHandleForCPU(0));
 
   int X_MIN = 0;
-  int X_MAX = 6000; //1000;
+  int X_MAX = 2000; //1000;
   int Y_MIN = 0;
-  int Y_MAX = 6000; //1000;
+  int Y_MAX = 2000; //1000;
   int STEP = 20; // Adjust based on your desired resolution
-  int tPixelDwelltime = 10;
+  int tPixelDwelltime = 0;
   int nFrames = 100;
   SPIRenderer *renderer = new SPIRenderer(X_MIN, X_MAX, Y_MIN, Y_MAX, STEP, tPixelDwelltime, nFrames);
   while (1){
